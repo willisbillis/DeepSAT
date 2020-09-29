bbab3e04c8bba0dc15bc1e61a28962e7cd3089c1
willisbillis <elliott.williams14@gmail.com>
2020-09-29 17:52:56 -0400

    Update evaluate_NB_model.py to use pandas instead of csv

--- NB_classifier/evaluate_NB_model.py
+++ NB_classifier/evaluate_NB_model.py
@@ -4,7 +4,6 @@ matplotlib.use('agg')
 import matplotlib.pyplot as plt
 import pickle
 import pandas as pd
-import csv
 import os
 
 from config_file_NB import Config
@@ -52,6 +51,7 @@ SENTIMENT_COUNTS = {"pos": 0, "neut": 0, "neg": 0}
 
 PRED_LIST = []
 HUMAN_REVIEW = []
+TRAINED_DATA = pd.read_csv(TRAINED_CSV)
 for test_review in TS.test_reviews_set:
     # predict outcome of review
     prob_result = CLASSIFIER.prob_classify(test_review["words"]).max()
@@ -70,27 +70,10 @@ for test_review in TS.test_reviews_set:
         if prob_result == "neutral":
             SENTIMENT_COUNTS[prob_result[:4]] += 1
 
-        with open(TRAINED_CSV, 'r') as csvfile:
-            ROWREADER = csv.reader(csvfile)
-
-            for row in ROWREADER:
-                if row[0] == str(test_review["ID"]) and len(row[2]) == 0:
-                    row[2] = prob_result.capitalize()
-                    PRED_LIST.append(row)
-
-with open(PREDICTED_CSV, 'w') as output:
-    HEADERS = ["ID", CONFIG.header_key, "SENTIMENT"]
-    WRITER = csv.DictWriter(output, fieldnames=HEADERS)
-    WRITER.writeheader()
-
-    SORTED_PRED_LIST = sorted(PRED_LIST, key=lambda x: int(x[0]))
-    for row in SORTED_PRED_LIST:
-        row_dict = {
-            "ID": row[0],
-            CONFIG.header_key: row[1],
-            "SENTIMENT": row[2]
-            }
-        WRITER.writerow(row_dict)
+        trained_class = TRAINED_DATA.loc[TRAINED_DATA[CONFIG.header_key] == test_review["sentence"], "SENTIMENT"]
+        if trained_class.hasnans:
+            TRAINED_DATA.loc[TRAINED_DATA[CONFIG.header_key] == test_review["sentence"], "SENTIMENT"] = prob_result.capitalize()
+        TRAINED_DATA.to_csv(PREDICTED_CSV, index=False)
 
 # Human Intervention - if predicted has low confidence or doesn't match
 #                      existing label provide a new label
@@ -105,55 +88,39 @@ if REVIEW_UNSURE == "y":
         prob_result = CLASSIFIER.prob_classify(test_review["words"]).max()
         confidence = CLASSIFIER.prob_classify(test_review["words"]).prob(prob_result)
 
-        with open(TRAINED_CSV, 'r') as csvfile:
-            ROWREADER = csv.DictReader(csvfile)
+        trained_class = TRAINED_DATA.loc[TRAINED_DATA[CONFIG.header_key] == test_review["sentence"], "SENTIMENT"]
+        print(trained_class.iloc[0])
+        if not trained_class.hasnans and trained_class.iloc[0] != prob_result.capitalize():
+            print("MISMATCH")
+        elif confidence < 0.5:
+            print("Predicted class confidence is low. Please verify or update class.")
+            print("To quit, enter 'Q'")
+            print("\n")
+            print(test_review["sentence"])
+            print("class: ", prob_result)
+            print("\n")
+            for idx, option in enumerate(CONFIG.class_options):
+                print(str(idx+1)+". "+option)
+            KEYBOARD_INPUT = input("Choose a class: ")
+            if KEYBOARD_INPUT == "Q":
+                break
 
-            for row in ROWREADER:
-                if row["ID"] == test_review["ID"]:
-                    if row["SENTIMENT"] and row["SENTIMENT"] != prob_result.capitalize():
-                        print("MISMATCH")
-                    elif confidence < 0.5:
-                        print("Predicted class confidence is low. Please verify or update class.")
-                        print("\n")
-                        print(test_review["sentence"])
-                        print("class: ", prob_result)
-                        print("\n")
-                        for idx, option in enumerate(CONFIG.class_options):
-                            print(str(idx+1)+". "+option)
-                        KEYBOARD_INPUT = input("Choose a class: ")
-                        row["SENTIMENT"] = CONFIG.class_options[int(KEYBOARD_INPUT)-1]
+            output_class = CONFIG.class_options[int(KEYBOARD_INPUT)-1]
 
-                        class_result = row["SENTIMENT"].lower()
+            TRAINED_DATA.loc[TRAINED_DATA[CONFIG.header_key] == test_review["sentence"], "SENTIMENT"] = output_class
+            TRAINED_DATA.to_csv(PREDICTED_CSV, index=False)
 
-                        if class_result in ("positive", "positivo"):
-                            OVERALL_SENTIMENT.append(1*confidence*100)
-                            SENTIMENT_COUNTS[class_result[:3]] += 1
-                        if class_result in ("negative", "negativo"):
-                            OVERALL_SENTIMENT.append(-1*confidence*100)
-                            SENTIMENT_COUNTS[class_result[:3]] += 1
-                        if class_result == "neutral":
-                            SENTIMENT_COUNTS[class_result[:4]] += 1
-                        clear()
-                    else:
-                        row["SENTIMENT"] = prob_result.capitalize()
+            class_result = output_class.lower()
 
-                    row_list = [row["ID"], row[CONFIG.header_key], row["SENTIMENT"]]
-                    PRED_LIST.append(row_list)
-
-    with open(PREDICTED_CSV, 'w') as output:
-        HEADERS = ["ID", CONFIG.header_key, "SENTIMENT"]
-        WRITER = csv.DictWriter(output, fieldnames=HEADERS)
-        WRITER.writeheader()
-
-        SORTED_PRED_LIST = sorted(PRED_LIST, key=lambda x: int(x[0]))
-        for row in SORTED_PRED_LIST:
-            row_dict = {
-                "ID": row[0],
-                CONFIG.header_key: row[1],
-                "SENTIMENT": row[2]
-                }
-            WRITER.writerow(row_dict)
-
+            if class_result in ("positive", "positivo"):
+                OVERALL_SENTIMENT.append(1*confidence*100)
+                SENTIMENT_COUNTS[class_result[:3]] += 1
+            if class_result in ("negative", "negativo"):
+                OVERALL_SENTIMENT.append(-1*confidence*100)
+                SENTIMENT_COUNTS[class_result[:3]] += 1
+            if class_result == "neutral":
+                SENTIMENT_COUNTS[class_result[:4]] += 1
+            clear()
 
 print("Plotting results...")
 # Plot sentiment distribution
